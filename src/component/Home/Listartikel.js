97ac3013565e25880878d239ca75d6b4016549e2
Taufik230301 <Taufiiqulhakim23@gmail.com>
2020-09-08 19:17:04 +0700

    Menambahkan link video Props

--- src/component/Home/Listartikel.js
+++ src/component/Home/Listartikel.js
@@ -6,9 +6,12 @@ class List extends React.Component{
     render(){
         return (
             <div className="Container1">
-             <h1>2020</h1>
+             <h1>React JS</h1>
              <NavLink href="https://www.youtube.com/watch?v=e-GuPT8nYc4">
                  <p className="list">Stateless dan StateFull Component pada ReactJS</p>
+            </NavLink>
+            <NavLink href="https://www.youtube.com/watch?v=8vLpcVfIScg&t=17s">
+                 <p className="list">Membuat Komponen yang dinamis Menggunakan PROPS pada ReactJS</p>
             </NavLink>
             </div>
         )
